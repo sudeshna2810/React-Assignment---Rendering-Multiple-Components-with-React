9b5702e83b0cca25a98274580de3b3519070dcb4
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-10 16:52:07 +0530

    Added Project Name and Description

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div>
+      <div data-ns-test="project-name">My First React Project</div>
+      <div data-ns-test="project-description">This is my First React Project.</div>
+    </div>
+    
   )
 }
 
